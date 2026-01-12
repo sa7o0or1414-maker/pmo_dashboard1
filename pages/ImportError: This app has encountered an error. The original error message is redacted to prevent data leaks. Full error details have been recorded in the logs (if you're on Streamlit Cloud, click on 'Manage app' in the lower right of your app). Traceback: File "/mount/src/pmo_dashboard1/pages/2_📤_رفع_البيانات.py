9de9e24162ddc929ba1fc96860643d9c521090f2
sa7o0or1414-maker <sa7o0or1414@gmail.com>
2026-01-12 13:04:00 +0300

    Update 2_📤_رفع_البيانات.py

--- pages/ImportError: This app has encountered an error. The original error message is redacted to prevent data leaks. Full error details have been recorded in the logs (if you're on Streamlit Cloud, click on 'Manage app' in the lower right of your app). Traceback: File "/mount/src/pmo_dashboard1/pages/2_📤_رفع_البيانات.py
+++ pages/ImportError: This app has encountered an error. The original error message is redacted to prevent data leaks. Full error details have been recorded in the logs (if you're on Streamlit Cloud, click on 'Manage app' in the lower right of your app). Traceback: File "/mount/src/pmo_dashboard1/pages/2_📤_رفع_البيانات.py
@@ -1,22 +1,18 @@
-import os
-from pathlib import Path
 import streamlit as st
+from pathlib import Path
+import os
 
 from utils.layout import sidebar_menu, page_title
 
 st.set_page_config(page_title="رفع البيانات", page_icon="📤", layout="wide")
+
 sidebar_menu(active="upload")
 page_title("📤 رفع البيانات")
 
-st.write("ارفعي ملف Excel وسيتم حفظه كـ **data/latest.xlsx** وتحديث لوحة المعلومات مباشرة.")
-
 uploaded = st.file_uploader("رفع ملف Excel", type=["xlsx"])
 
-if uploaded is not None:
-    Path("data").mkdir(parents=True, exist_ok=True)
-    save_path = os.path.join("data", "latest.xlsx")
-
-    with open(save_path, "wb") as f:
+if uploaded:
+    Path("data").mkdir(exist_ok=True)
+    with open("data/latest.xlsx", "wb") as f:
         f.write(uploaded.getbuffer())
-
-    st.success("✅ تم رفع الملف بنجاح. انتقلي إلى (🏠 الصفحة الرئيسية) لرؤية الداشبورد.")
+    st.success("تم رفع الملف بنجاح ✅ انتقلي للداشبورد لمشاهدة التحليل")
