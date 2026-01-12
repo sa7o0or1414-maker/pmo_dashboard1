ace906d21aa8ee39944b5aeff0e862cb6b022425
sa7o0or1414-maker <sa7o0or1414@gmail.com>
2026-01-12 13:01:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title="الصفحة الرئيسية", page_icon="🏠", layout="wide")
+st.set_page_config(
+    page_title="لوحة المعلومات",
+    page_icon="📊",
+    layout="wide"
+)
 
+# تحويل مباشر للداشبورد
+st.switch_page("pages/1_🏠_الداشبورد.py")
