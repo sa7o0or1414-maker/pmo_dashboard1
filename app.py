af9418b8f91322d33c7cfcbb433c34723821f3c8
sa7o0or1414-maker <sa7o0or1414@gmail.com>
2026-01-12 12:38:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,3 @@ import streamlit as st
 
 st.set_page_config(page_title="الصفحة الرئيسية", page_icon="🏠", layout="wide")
 
-# دائمًا افتح الصفحة الرئيسية من pages
-st.switch_page("pages/1_🏠_الصفحة_الرئيسية.py")
